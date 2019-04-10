ffe96b72c772d4d111da394dfc2be7c074fc4998
huymirale96 <huymin96@gmail.com>
2019-04-11 00:58:31 +0700

    Update FormSinhVien.cs

--- BTL_QuanLyThiTracNghiem/FormSinhVien.cs
+++ BTL_QuanLyThiTracNghiem/FormSinhVien.cs
@@ -11,7 +11,7 @@ using System.Data.SqlClient;
 //sua tren git nga
 //sua local1pok
 //edit from git
-//okk23
+//okk2333
 namespace BTL_QuanLyThiTracNghiem
 {
     public partial class FormSinhVien : Form
