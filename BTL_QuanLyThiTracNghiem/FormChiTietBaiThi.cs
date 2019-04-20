ff2736f2b9488fef24c835b7a3ca04dc3c39d914
Huy dzz <Huy dzz@DESKTOP-4TI11EU>
2019-04-21 00:50:34 +0700

    hi

--- BTL_QuanLyThiTracNghiem/FormChiTietBaiThi.cs
+++ BTL_QuanLyThiTracNghiem/FormChiTietBaiThi.cs
@@ -15,6 +15,20 @@ namespace BTL_QuanLyThiTracNghiem
         public FormChiTietBaiThi()
         {
             InitializeComponent();
+           // Load();
         }
+       /* public void Load()
+        {
+            string s = "";
+            int temp;
+            Random rd = new Random();
+            temp = rd.Next(1,20);
+            s = "sbd" + temp.ToString();
+            label1.Text = s;
+            string myString = "003.3";
+            float myInt = float.Parse(myString);
+            label2.Text = myInt.ToString();
+        }
+        */
     }
 }
